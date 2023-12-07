d91809269fa89b6cc009d9a8f42088bb247cbad2
Jan Hrcan <jankochrtan555@gmail.com>
2023-12-07 18:16:23 +0100

    style: ui change

---
+++ app/src/main/java/eu/mcomputing/mobv/mobvzadanie/fragments/ForgotPasswordFragment.kt
@@ -0,0 +1,12 @@
+package eu.mcomputing.mobv.mobvzadanie.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import eu.mcomputing.mobv.mobvzadanie.R
+
+class ForgotPasswordFragment : Fragment(R.layout.fragment_forgot_password) {
+
+}
---
+++ app/src/main/java/eu/mcomputing/mobv/mobvzadanie/fragments/ResetPasswordFragment.kt
@@ -0,0 +1,12 @@
+package eu.mcomputing.mobv.mobvzadanie.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import eu.mcomputing.mobv.mobvzadanie.R
+
+class ResetPasswordFragment : Fragment(R.layout.fragment_reset_password) {
+
+}
